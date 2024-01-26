ef46fe9e005a230e0c3c73527fda2f8e3075d631
habtew16 <habtamuwolde96@gmail.com>
2024-01-25 23:50:11 -0800

    fixed error

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,32 +6,25 @@ archive to the web servers
 
 from fabric.api import put, run, env
 from os.path import exists
-
-env.hosts = ['100.25.119.157', '100.26.122.231']
+env.hosts = ['54.89.109.87', '100.25.190.21']
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to the web servers"""
-    if not exists(archive_path):
-        print("Error: Archive not found.")
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
         return False
-
     try:
-        file_name = archive_path.split("/")[-1]
-        no_extension = file_name.split(".")[0]
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
         path = "/data/web_static/releases/"
-
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_extension))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_name, path, no_extension))
-        run('rm /tmp/{}'.format(file_name))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_extension))
-        run('rm -rf {}{}/web_static'.format(path, no_extension))
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_extension))
-        print("Deployment successful!")
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
         return True
-
-    except Exception as e:
-        print("Error during deployment: {}".format(e))
+    except:
         return False
