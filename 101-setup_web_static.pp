0bb84e8f8ecb70ebce1fb51779049381e07f8386
habtew16 <habtamuwolde96@gmail.com>
2024-01-26 08:14:49 -0800

    fixed error

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -12,7 +12,7 @@ $nginx_conf = "server {
         index index.html index.htm;
     }
     location /redirect_me {
-        return 301 http://github.com/besthor/;
+        return 301 http://github.com/habtew/;
     }
     error_page 404 /404.html;
     location /404 {
