c0deb29b4d61ed1dc3872e206a06a69a43aed3cb
habtew16 <habtamuwolde96@gmail.com>
2024-01-25 23:37:33 -0800

    py zip creator

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,25 +1,23 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
-fabric script to generate zip file
-of the web static
+Fabric script to genereate tgz archive
+execute: fab -f 1-pack_web_static.py do_pack
 """
 
 from datetime import datetime
-from fabric.api import local
+from fabric.api import *
 
 
 def do_pack():
     """
-    function to create zip
+    making an archive on web_static folder
     """
+
     time = datetime.now()
     archive = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
     local('mkdir -p versions')
-    
-    # Run the tar command and check the exit code
-    result = local('tar -cvzf versions/{} web_static'.format(archive), capture=True)
-    
-    if result.succeeded:
-        return 'versions/{}'.format(archive)
+    create = local('tar -cvzf versions/{} web_static'.format(archive))
+    if create is not None:
+        return archive
     else:
         return None
