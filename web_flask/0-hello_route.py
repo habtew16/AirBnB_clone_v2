d3ba7ee0e6135a009dbe403ba2ef3058e7142f6e
habtew16 <habtamuwolde96@gmail.com>
2024-01-28 00:11:31 -0800

    flask start

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# start flask server
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/',  strict_slashes=False)
+def hello_world():
+    # print hello world with flask
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
