8c42320b0e710efa73f9b67e64e648ba26375aad
habtew16 <habtamuwolde96@gmail.com>
2024-01-28 01:59:27 -0800

    check if number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+starts flask with python text
+"""
+from flask import Flask, abort
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def ctext(text):
+    return 'C ' + text.replace("_", ' ')
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def ptext(text='is cool'):
+    return 'Python {}'.format(text.replace("_", ' '))
+
+
+@app.route('/number/<n>')
+def is_number(n):
+    try:
+        return '{} is a number'.format(int(n))
+    except:
+        abort(n)
+
+
+if __name__ == "__main__":
+    app.url_map.strict_slashes = False
+    app.run(host="0.0.0.0", port=5000)
