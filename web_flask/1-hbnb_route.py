a05fd3070e0e16ea9eddd0cf9afc8eef769cdecc
habtew16 <habtamuwolde96@gmail.com>
2024-01-28 01:03:36 -0800

    added task zero in tsk 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -5,11 +5,18 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route('/')
+def hello_world():
+    # prints hello
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
 def hello_world():
     # prints hbnb
     return "HBNB!"
 
 
 if __name__ == "__main__":
+    app.url_map.strict_slashes = False
     app.run(host="0.0.0.0", port=5000)
