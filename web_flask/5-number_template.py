77cec9b65a6a2378dfedf34b9525e9ab43bb953b
habtew16 <habtamuwolde96@gmail.com>
2024-01-28 02:31:35 -0800

    fixed assignment error

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+starts flask with python text
+"""
+from flask import Flask, abort, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def ctext(text):
+    return 'C ' + text.replace("_", ' ')
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def ptext(text='is cool'):
+    return 'Python {}'.format(text.replace("_", ' '))
+
+
+@app.route('/number/<int:n>')
+def is_number(n):
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<n>')
+def num_template(n):
+    return render_template('5-number.html', number=n)
+
+
+if __name__ == "__main__":
+    app.url_map.strict_slashes = False
+    app.run(host="0.0.0.0", port=5000)
